34519ea98824c92745db62d20ee4ca946a4a90dd
Shanmukha Harshith <ksharshith3@gmail.com>
2019-06-01 19:38:36 +0530

    Made sections size changable. It's flexible now.

--- quiz_v2.c
+++ quiz_v2.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define MAX_QUESTIONS 100
 #define SCORE_POINTS 10
-#define SECTION_SIZE 2
+#define MAX_SECTION_SIZE 5
+#define CATEGORY_COUNT 3
+#define CATEGORY_NAMES {"General Knowledge", "Music", "Sports"}
 
 struct question {
 	int category;
@@ -17,8 +19,13 @@ struct question {
 };
 
 int main() {
+
+	// Time for seed of random number
+	time_t time_value;
+	// For readin line from file
 	ssize_t read_size;
 	size_t len = 0;
+
 	char *temp_line = NULL;
 	FILE *questions_file;
 	struct question questions[MAX_QUESTIONS];
@@ -27,6 +34,7 @@ int main() {
 	unsigned int total_score = 0;
 	unsigned int correct_answers_count = 0;
 	unsigned int wrong_answers_count = 0;
+	char* question_categories[CATEGORY_COUNT] = CATEGORY_NAMES;
 
 	questions_file = fopen("questions_v2.txt", "r");
 	if (questions_file) {
@@ -34,72 +42,56 @@ int main() {
 		for (int i = 0; i < MAX_QUESTIONS; i++) {
 			// Reading the category of the question
 			if((read_size = getline(&temp_char, &len, questions_file)) == -1) {
-				printf("reached end of questions_file\n");
+				printf("ERROR: Reached end of questions_file without reading the category.\n");
 				break;
 			} else {
 				questions[i].category = *temp_char - '0';
-				/* printf("entered: %d\n", questions[i].category); */
 			}
 
 			// Reading the question
 			if((read_size = getline(&questions[i].statement, &len, questions_file)) == -1) {
-				printf("reached end of questions_file\n");
+				printf("ERROR: Reached end of questions_file without reading the question.\n");
 				break;
 			}
-			/* else { */
-			/* printf("entered: %s\n", questions[i].statement); */
-			/* } */
 
 			// Reading the correct answer
 			if((read_size = getline(&temp_char, &len, questions_file)) == -1) {
-				printf("reached end of questions_file\n");
+				printf("ERROR: Reached end of questions_file without reading the correct answer.\n");
 				break;
 			} else {
 				questions[i].answer = *temp_char - '0';
-				/* printf("entered: %d\n", questions[i].answer); */
 			}
 
 			// Reading option 1
 			if((read_size = getline(&questions[i].option_1, &len, questions_file)) == -1) {
-				printf("reached end of questions_file\n");
+				printf("ERROR: Reached end of questions_file without reading option_1.\n");
 				break;
 			}
-			/* else { */
-			/* printf("entered: %s\n", questions[i].option_1); */
-			/* } */
 
 			// Reading option 2
 			if((read_size = getline(&questions[i].option_2, &len, questions_file)) == -1) {
-				printf("reached end of questions_file\n");
+				printf("ERROR: Reached end of questions_file without reading option_2.\n");
 				break;
 			}
-			/* else { */
-			/* printf("entered: %s\n", questions[i].option_2); */
-			/* } */
 
 			// Reading option 3
 			if((read_size = getline(&questions[i].option_3, &len, questions_file)) == -1) {
-				printf("reached end of questions_file\n");
+				printf("ERROR: Reached end of questions_file without reading option_3.\n");
 				break;
 			}
-			/* else { */
-			/* printf("entered: %s\n", questions[i].option_3); */
-			/* } */
 
 			// Reading option 4
 			if((read_size = getline(&questions[i].option_4, &len, questions_file)) == -1) {
-				printf("reached end of questions_file\n");
+				// completed reading a question along with all the options
+				printf("ERROR: Reached end of questions_file without reading option_4.\n");
 				break;
 			}
-			/* else { */
-			/* printf("entered: %s\n", questions[i].option_4); */
-			/* } */
 
 			questions_read++;
 
 			// Reading empty space (this also serves as a format check)
 			if((read_size = getline(&temp_line, &len, questions_file)) == -1) {
-				printf("reached end of questions_file\n");
+				printf("Questions loaded successfully. Your Quiz beings now.\n");
 				break;
 			} else {
 				// 10 is the newline character thingy. At least in linux, don't know about windows.
@@ -112,73 +104,57 @@ int main() {
 	}
 	fclose(questions_file);
 
-	// We will seperate the question into different array based on the sections
-	
-	struct question section_1_questions[SECTION_SIZE];
-	struct question section_2_questions[SECTION_SIZE];
-	struct question section_3_questions[SECTION_SIZE];
-	int section_1_index = 0;
-	int section_2_index = 0;
-	int section_3_index = 0;
+	// We will seperate the questions into different array based on the sections
+	struct question divided_questions[CATEGORY_COUNT][MAX_SECTION_SIZE];
+	int divided_questions_sizes[CATEGORY_COUNT];
 
-	for (int i = 0; i < questions_read; i++) {
-		if (questions[i].category == 1){
-			section_1_questions[section_1_index] = questions[i];
-			section_1_index++;
-		} else if (questions[i].category == 2){
-			section_2_questions[section_2_index] = questions[i];
-			section_2_index++;
-		} else if (questions[i].category == 3){
-			section_3_questions[section_3_index] = questions[i];
-			section_3_index++;
+	for (int j = 0; j < CATEGORY_COUNT; j++) {
+		struct question temp_category_questions[MAX_SECTION_SIZE];
+		int temp_category_size = 0;
+		for (int i = 0; i < questions_read; i++) {
+			if (questions[i].category == j+1) {
+				divided_questions[j][temp_category_size] = questions[i];
+				temp_category_size++;
+			}
+		}
+		divided_questions_sizes[j] = temp_category_size;
+	}
+
+	printf("Questions divided");
+	for (int i=0; i<CATEGORY_COUNT; i++){
+		printf("Category: %s\n", question_categories[i]);
+		for (int j=0; j<divided_questions_sizes[i]; j++){
+			printf("\t%s", divided_questions[i][j].statement);
 		}
 	}
 
-	// Time for seed of random number
-	time_t t;
+	for (int q = 0; q < CATEGORY_COUNT; q++) {
+		printf("\n=================== %s section =============================\n", question_categories[q]);
 
-	// Initializing random number generator
-	srand((unsigned) time(&t));
+		int sequence[divided_questions_sizes[q]];
 
-	struct question* divided_questions[3];
-	divided_questions[0] = section_1_questions;
-	divided_questions[1] = section_2_questions;
-	divided_questions[2] = section_3_questions;
-	int divided_questions_indicies[3] = {section_1_index, section_2_index, section_3_index};
-
-/*
- *     for(int q=0; q< 3; q++){
- *         printf("SECTION %d\n", q);
- * 
- *         for(int p=0; p < 2; p++){
- *             printf("\nQuestion: %s", divided_questions[q][p].statement);
- *         }
- *     }
- */
-
-	for (int q = 0; q < 3; q++) {
-		printf("\n=============== Section %d ===========\n", q + 1);
-
-		int sequence[SECTION_SIZE];
 		// Populating the array
-		for (int i = 0; i < SECTION_SIZE; i++) {
+		for (int i = 0; i < divided_questions_sizes[q]; i++) {
 			sequence[i] = i;
 		}
 
+	// Initializing random number generator
+	srand((unsigned) time(&time_value));
+
 		// Shuffling the elements of the array
-		for (int i = 0; i < SECTION_SIZE; i++) {
+		for (int i = 0; i < divided_questions_sizes[q]; i++) {
 			int temp_element = sequence[i];
-			int random_index = rand() % SECTION_SIZE;
+			int random_index = rand() % divided_questions_sizes[q];
 			sequence[i] = sequence[random_index];
 			sequence[random_index] = temp_element;
 		}
 
-		for(int j = 0; j < divided_questions_indicies[q]; j++) {
-			printf("\nQuestion: %s", divided_questions[q][sequence[j]].statement);
-			printf("1: %s", divided_questions[q][sequence[j]].option_1);
-			printf("2: %s", divided_questions[q][sequence[j]].option_2);
-			printf("3: %s", divided_questions[q][sequence[j]].option_3);
-			printf("4: %s", divided_questions[q][sequence[j]].option_4);
+		for(int j = 0; j < divided_questions_sizes[q]; j++) {
+			printf("\nQuestion %d: %s", j+1, divided_questions[q][sequence[j]].statement);
+			printf("\t1) %s", divided_questions[q][sequence[j]].option_1);
+			printf("\t2) %s", divided_questions[q][sequence[j]].option_2);
+			printf("\t3) %s", divided_questions[q][sequence[j]].option_3);
+			printf("\t4) %s", divided_questions[q][sequence[j]].option_4);
 
 			while (entered_option < 1 || entered_option > 4) {
 				printf("Please enter your answer (1, 2, 3 or 4): ");
@@ -197,14 +173,16 @@ int main() {
 			entered_option = -1;
 		}
 
-		printf("You have completed this section. Now you'll be moving to the next section.\n");
+		printf("You have completed this section successfully!\n");
 	}
 
-	printf("\n\n======================================\n");
-	printf("======================================\n");
+	printf("\n\n================================================================================\n");
+	printf("================================================================================\n");
+
 	printf("Thanks for taking the quiz. You've answered %d questions correctly and %d questions incorrectly.\nYour total score is %d.\n", correct_answers_count, wrong_answers_count, total_score);
-	printf("======================================\n");
-	printf("======================================\n");
+
+	printf("================================================================================\n");
+	printf("================================================================================\n");
 
 	return 0;
 }
